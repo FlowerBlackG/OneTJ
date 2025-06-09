fa0e983622803146a6f90b7d766c97b624f6ed3f
Flower Black <fb2466375479@outlook.com>
2025-06-09 16:45:19 +0800

    Update maven mirrors.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,17 @@
 pluginManagement {
     repositories {
+        maven { url = uri("https://mirrors.huaweicloud.com/repository/maven/") }
+        maven { url = uri("https://mirrors.cloud.tencent.com/maven/") }
         google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven { url = uri("https://mirrors.huaweicloud.com/repository/maven/") }
+        maven { url = uri("https://mirrors.cloud.tencent.com/maven/") }
         google()
         mavenCentral()
     }
