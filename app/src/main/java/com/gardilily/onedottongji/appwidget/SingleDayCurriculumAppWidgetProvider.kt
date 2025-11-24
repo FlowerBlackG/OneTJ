417f3b6ff27912b8e1d565e1aac60ee18ddf98df
oierxjn <1934672944@qq.com>
2025-11-24 19:51:02 +0800

    Feat: 完成小组件功能，能够进行基础的课程信息显示

--- app/src/main/java/com/gardilily/onedottongji/appwidget/SingleDayCurriculumAppWidgetProvider.kt
+++ app/src/main/java/com/gardilily/onedottongji/appwidget/SingleDayCurriculumAppWidgetProvider.kt
@@ -11,8 +11,62 @@ import android.util.Log
 import android.widget.RemoteViews
 import com.gardilily.onedottongji.R
 import com.gardilily.onedottongji.service.SingleDayCurriculumAppWidgetGridContainerService
+import com.gardilily.onedottongji.service.SingleDayCurriculumAppWidgetGridContainerService.CourseInfo
+import com.gardilily.onedottongji.tools.ProxyActivity
+import com.gardilily.onedottongji.tools.tongjiapi.TongjiApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import java.util.Calendar
+import kotlin.text.ifEmpty
 
 class SingleDayCurriculumAppWidgetProvider : AppWidgetProvider() {
+
+    /**
+     * 通过输入的参数返回当天的课程
+     */
+    fun getTodayCourseInfo(week: Int, dayOfWeek: Int, json: JSONArray?): List<CourseInfo> {
+        if (json == null) return emptyList()
+        val courses = mutableListOf<CourseInfo>()
+        for (i in 0 until json.length()) {
+            try {
+                /**
+                 * timeTableList包含对应课程的所有节次
+                 */
+                val timeTableList = json.getJSONObject(i).getJSONArray("timeTableList")
+                for (j in 0 until timeTableList.length()) {
+                    /**
+                     * 一周固定的某节课
+                     */
+                    val courseObj = timeTableList.getJSONObject(j)
+                    val weeks = courseObj.getJSONArray("weeks")
+                    if (courseObj.getInt("dayOfWeek") != dayOfWeek) { continue }
+
+                    for (k in 0 until weeks.length()) {
+                        val weekObj = weeks.getInt(k)
+                        if (weekObj == week) {
+                            courses.add(
+                                CourseInfo(
+                                    timeStart = courseObj.getInt("timeStart"),
+                                    timeEnd = courseObj.getInt("timeEnd"),
+                                    name = courseObj.getString("courseName"),
+                                    teacher = courseObj.getString("teacherName"),
+                                    room = courseObj.getString("roomIdI18n").ifEmpty { courseObj.getString("roomLable") }
+                                )
+                            )
+                        }
+                    }
+
+                }
+            } catch (_: Exception) { /* Ignore parsing errors */ }
+        }
+        return courses.sortedBy { it.timeStart }
+    }
+
 
     override fun onUpdate(
         context: Context?,
@@ -25,17 +79,55 @@ class SingleDayCurriculumAppWidgetProvider : AppWidgetProvider() {
 
         Log.d("single day curriculum app widget", "onUpdate tick")
 
-        appWidgetIds.forEach { appWidgetId ->
-            val views = RemoteViews(context.packageName, R.layout.appwidget_single_day_curriculum)
+        CoroutineScope(Dispatchers.IO + SupervisorJob()).launch {
+            val proxyActivity1 = withContext(Dispatchers.Main) { ProxyActivity.create(context) }
+            val proxyActivity2 = withContext(Dispatchers.Main) { ProxyActivity.create(context) }
+            val calendarDeferred = async(Dispatchers.IO) {
+                return@async try {
+                    TongjiApi.instance.getOneTongjiSchoolCalendar(proxyActivity1)
+                } catch (e: Exception) {
+                    Log.e("RemoteViewsFactory", "校历请求失败：${e.message}")
+                    null
+                }
+            }
 
-            val intent = Intent(context, SingleDayCurriculumAppWidgetGridContainerService::class.java)
-            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+            val timetableDeferred = async(Dispatchers.IO) {
+                return@async try {
+                    TongjiApi.instance.getOneTongjiStudentTimetable(proxyActivity2)
+                } catch (e: Exception) {
+                    Log.e("RemoteViewsFactory", "课程表请求失败：${e.message}")
+                    null
+                }
+            }
 
-            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)))
+            val calendarData = calendarDeferred.await()
+            val timetableData = timetableDeferred.await()
 
-            views.setRemoteAdapter(R.id.appwidget_single_day_curriculum_card_container, intent)
+            val todayWeek = calendarData?.schoolWeek?.toInt()
+            val todayDayOfWeek = (Calendar.getInstance().get(Calendar.DAY_OF_WEEK)-1).let { if (it < 0) 6 else it }
 
-            appWidgetManager.updateAppWidget(appWidgetId, views)
+//            Log.d("RemoteViewsFactory", "请求完成。今天是第${todayWeek}周，星期${todayDayOfWeek}")
+
+            val infoList = getTodayCourseInfo(todayWeek ?: 1, todayDayOfWeek, timetableData)
+
+            /* 数据写入服务块 */
+            SingleDayCurriculumAppWidgetGridContainerService.infoList = infoList
+            SingleDayCurriculumAppWidgetGridContainerService.isDataLoaded = true
+
+            withContext(Dispatchers.Main) {
+                appWidgetIds.forEach { appWidgetId ->
+                    val views = RemoteViews(context.packageName, R.layout.appwidget_single_day_curriculum)
+
+                    val intent = Intent(context, SingleDayCurriculumAppWidgetGridContainerService::class.java)
+                    intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+
+                    intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)))
+
+                    views.setRemoteAdapter(R.id.appwidget_single_day_curriculum_card_container, intent)
+
+                    appWidgetManager.updateAppWidget(appWidgetId, views)
+                }
+            }
         }
 
     }
@@ -45,5 +137,7 @@ class SingleDayCurriculumAppWidgetProvider : AppWidgetProvider() {
 
         Log.d("single day curri widget provider", "onReceive tick")
     }
+
+
 
 }
--- app/src/main/java/com/gardilily/onedottongji/service/SingleDayCurriculumAppWidgetGridContainerService.kt
+++ app/src/main/java/com/gardilily/onedottongji/service/SingleDayCurriculumAppWidgetGridContainerService.kt
@@ -2,17 +2,38 @@
 
 package com.gardilily.onedottongji.service
 
+import android.app.Activity
 import android.appwidget.AppWidgetManager
 import android.content.Context
 import android.content.Intent
+import android.util.Log
+import android.view.View
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
 import com.gardilily.onedottongji.R
+import com.gardilily.onedottongji.tools.ProxyActivity
+import com.gardilily.onedottongji.tools.tongjiapi.TongjiApi
+import com.gardilily.onedottongji.tools.tongjiapi.TongjiApi.Companion.BASE_URL
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
+import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import java.util.Calendar
 
 class SingleDayCurriculumAppWidgetGridContainerService : RemoteViewsService() {
 
     override fun onGetViewFactory(intent: Intent?): RemoteViewsFactory {
         return SingleDayCurriculumAppWidgetGridContainerRemoteViewsFactory(this, intent!!)
+    }
+    companion object{
+        @Volatile
+        var infoList: List<CourseInfo> = emptyList()
+        @Volatile
+        var isDataLoaded: Boolean = false
     }
 
     data class CourseInfo(
@@ -21,15 +42,15 @@ class SingleDayCurriculumAppWidgetGridContainerService : RemoteViewsService() {
         var timeEnd: Int = 1,
         var courseNumber: String = "",
         var teacher: String = "",
+        var room: String = ""
     )
 
     protected class SingleDayCurriculumAppWidgetGridContainerRemoteViewsFactory(
         val context: Context,
         val intent: Intent
-    ) : RemoteViewsService.RemoteViewsFactory {
+    ) : RemoteViewsFactory {
         val appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID)
 
-        val infoList = ArrayList<CourseInfo>()
 
         override fun onCreate() {
 
@@ -44,12 +65,31 @@ class SingleDayCurriculumAppWidgetGridContainerService : RemoteViewsService() {
         }
 
         override fun getCount(): Int {
-            return infoList.size + 10
+            return if (infoList.isEmpty()) 1 else infoList.size
         }
 
         override fun getViewAt(position: Int): RemoteViews {
             val remoteView = RemoteViews(context.packageName, R.layout.appwidget_singledaycurriculum_item)
-            remoteView.setTextViewText(R.id.appwidget_singledaycurriculum_grid_item, "${System.currentTimeMillis()}") // todo
+            //列表为空的提示
+            if (infoList.isEmpty()){
+                remoteView.setViewVisibility(R.id.widget_course_name, View.GONE)
+                remoteView.setViewVisibility(R.id.widget_course_room, View.GONE)
+                remoteView.setViewVisibility(R.id.widget_course_teacher, View.GONE)
+                remoteView.setViewVisibility(R.id.widget_course_time, View.GONE)
+
+                remoteView.setViewVisibility(R.id.widget_course_empty, View.VISIBLE)
+                if (isDataLoaded){
+                    remoteView.setTextViewText(R.id.widget_course_empty, "今天没有课！")
+                }else{
+                    remoteView.setTextViewText(R.id.widget_course_empty, "正在加载课程表...")
+                }
+            }else{
+                val course = infoList[position]
+                remoteView.setTextViewText(R.id.widget_course_name, course.name)
+                remoteView.setTextViewText(R.id.widget_course_room, course.room)
+                remoteView.setTextViewText(R.id.widget_course_teacher, course.teacher)
+                remoteView.setTextViewText(R.id.widget_course_time, "${course.timeStart}-${course.timeEnd}")
+            }
 
             return remoteView
         }
@@ -81,3 +121,4 @@ class SingleDayCurriculumAppWidgetGridContainerService : RemoteViewsService() {
         super.onDestroy()
     }
 }
+
